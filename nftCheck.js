1159382bdd1af8b474a7920f896e09a9fb3179da
Oxford Developer <141911139+oxforddev@users.noreply.github.com>
2026-05-24 02:09:11 +0530

    Refactor NFT check function and update collection address

--- nftCheck.js
+++ nftCheck.js
@@ -1,53 +1,45 @@
 const axios = require("axios");
 
-// YOUR NFT COLLECTION ADDRESS
-const COLLECTION = "0:40afcfa41fc6744742bd1fc887d27760546b25418e296f811e611c7f7c721df5";
+const COLLECTION =
+  "EQBAr8-kH8Z0R0K9H8iH0ndgVGslQY4pb4EeYRx_fHId9cxA";
 
 async function checkNFT(wallet) {
 
   try {
 
-    console.log("Checking wallet:", wallet);
-
     const url =
       `https://tonapi.io/v2/accounts/${wallet}/nfts`;
 
-    const response = await axios.get(url);
+    const res = await axios.get(url);
 
-    const nfts = response.data.nft_items || [];
+    const nfts = res.data.nft_items || [];
 
     console.log("NFT COUNT:", nfts.length);
 
-    // DEBUG ALL NFT COLLECTIONS
-    nfts.forEach((nft, index) => {
+    for (const nft of nfts) {
 
-      console.log(
-        `NFT ${index}:`,
-        nft.collection?.address
-      );
+      const col =
+        nft.collection?.address ||
+        nft.collection_address ||
+        nft.collection;
 
-    });
+      console.log("COLLECTION FOUND:", col);
 
-    const ownsNFT = nfts.some((nft) => {
+      if (
+        col &&
+        col.toLowerCase() === COLLECTION.toLowerCase()
+      ) {
+        return true;
+      }
+    }
 
-      return (
-        nft.collection &&
-        nft.collection.address &&
-        nft.collection.address.toLowerCase() ===
-        COLLECTION.toLowerCase()
-      );
+    return false;
 
-    });
-
-    console.log("NFT VERIFIED:", ownsNFT);
-
-    return ownsNFT;
-
-  } catch (error) {
+  } catch (err) {
 
     console.log(
-      "NFT check error:",
-      error.response?.data || error.message
+      "NFT ERROR:",
+      err.response?.data || err.message
     );
 
     return false;
